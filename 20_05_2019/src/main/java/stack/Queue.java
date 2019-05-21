a185e3be9c2232fdeaa8261f3af6c619475b6bf8
Octav <OctavSwDev@gmail.com>
2019-05-21 21:29:37 +0300

    Scoala

--- 20_05_2019/src/main/java/stack/Queue.java
+++ 20_05_2019/src/main/java/stack/Queue.java
@@ -1,16 +1,34 @@
 package stack;
 
 public class Queue {
-    private int firstElem = 0;
-    private int lastElem = 0;
-    private int[] queue;
+//    private int firstElem = 0;
+//    private int lastElem = 0;
+//    private int[] queue;
+//
+//    public Queue(int capacity){
+//        queue = new int[capacity];
+//    }
+//
+//    public String toString(){
+//        queue
+//    }
+//}
 
-    public Queue(int capacity){
-        queue = new int[capacity];
+    private int[] array;
+    private int front;
+    private int back;
+
+    public Queue(int maxSize) {
+        this.array = new int[maxSize];
+        front = back = 0;
     }
 
-    public String toString(){
-        queue 
+    public void enqueue(int elem) {
+        if (back < array.length) {
+            array[back++] = elem;
+        }
     }
 
+
+    public String toString() { return "Front = " + front + "Back = " + back + }
 }
--- 20_05_2019/src/test/java/stack/QueueTest.java
+++ 20_05_2019/src/test/java/stack/QueueTest.java
@@ -1,4 +1,14 @@
 package stack;
 
+import org.junit.Test;
+
 public class QueueTest {
+
+    @Test
+    public void testEnqueueDequeue(){
+        Queue queue = new Queue(3);
+        queue.enqueue(1);
+        queue.enqueue(12);
+    }
+
 }
